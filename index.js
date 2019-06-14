66383b750d69e8784e54ae161c7160ce633511b9
brittjohn87 <brittanytjohnson@icloud.com>
2019-06-14 16:37:43 -0400

    boilerplate push

--- index.js
+++ index.js
@@ -1,45 +1,59 @@
 $("#8ball").click(function(){
-	prompt("Ask Me Anything?");
-    $("#8ball").fadeOut("fast");
+    prompt("Ask Me Anything?");
     eightBall();
+    reset8ball();
 });
 
 function eightBall(){
 
     /**
      * Answer Key
-     * 1-10 = Signs Point To Yes
-     * 11-20 = Reply Hazy, Try Again Later
-     * 21-30 = My Sources Say No
-     * 31-40 = Without A Doubt
-     * 41-50 = Don't Count On It
-     * 51-60 = Concentrate And Ask Again
+     * 1-10 = 
+     * 11-20 = 
+     * 21-30 = 
+     * 31-40 = 
+     * 41-50 = 
+     * 51-60 = 
+     */
+
+    /**
+     * Phrases
+     * Signs Point To Yes
+     * Without A Doubt
+     * Reply Hazy, Try Again Later
+     * Concentrate And Ask Again
+     * My Sources Say No
+     * Don't Count On It
      */
 
     var number = Math.floor(Math.random() * 60) + 1;
 
-	// if (){
-		$('#response').html("Signs Point To Yes!");
+	if (){
+		$('#response').html();
 	}
 
-	// else if (){
-		$('#response').html("Reply Hazy, Try Again Later")
+	else if (){
+		$('#response').html()
 	}
 
-	// else if (){
-		$('#response').html("My Sources Say No")
+    else if (){
+		$('#response').html()
 	}
 
-	// else if (){
-		$('#response').html("Without A Doubt")
+	else if (){
+		$('#response').html()
     }
-    // else if (){
-        $('#response').html("Don't Count On It")
+    else if (){
+        $('#response').html()
     }
-    // else {
-        $('#response').html("Concentrate And Ask Again")
+    else {
+        $('#response').html()
     }
     console.log(number);
-    $('#answer').fadeIn("slow")
+    $("#8ball").fadeOut(300);
+    $('#answer').delay(300).fadeIn(200)
 }
 
+function reset8ball() {
+   // a function to reset our 8ball 
+}
